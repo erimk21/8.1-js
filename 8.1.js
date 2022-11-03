3a92e90d0cb6a84778a6ce2beba8c80194c1b535
erimk21 <9019259@student.zadkine.nl>
2022-11-03 21:32:22 +0100

    opzet 8.1-js

---
+++ 8.1.js
@@ -0,0 +1,28 @@
+function plus(){
+
+    var getal1 = parseInt(window.document.getElementById("getal1").value, 10);
+    var getal2 = parseInt(window.document.getElementById("getal2").value, 10);
+    let antwoord = getal1+getal2;
+    document.getElementById("antwoord").innerHTML = antwoord;
+}
+function min(){
+
+    var getal1 = parseInt(window.document.getElementById("getal1").value, 10);
+    var getal2 = parseInt(window.document.getElementById("getal2").value, 10);
+    let antwoord = getal1-getal2;
+    document.getElementById("antwoord").innerHTML = antwoord;
+}
+function keer(){
+
+    var getal1 = parseInt(window.document.getElementById("getal1").value, 10);
+    var getal2 = parseInt(window.document.getElementById("getal2").value, 10);
+    let antwoord = getal1-getal2;
+    document.getElementById("antwoord").innerHTML = antwoord;
+}
+function delen(){
+
+    var getal1 = parseInt(window.document.getElementById("getal1").value, 10);
+    var getal2 = parseInt(window.document.getElementById("getal2").value, 10);
+    let antwoord = getal1-getal2;
+    document.getElementById("antwoord").innerHTML = antwoord;
+}
